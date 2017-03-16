ff32d02bdce1eb15097a0f40f28ccff65fb51fdf
Beatrix <morozbea@gmail.com>
2017-03-16 12:10:35 +0100

    frågesport en del färdig

--- GameForSeniors/Assets/Scripts/QuestionSCript.cs
+++ GameForSeniors/Assets/Scripts/QuestionSCript.cs
@@ -16,12 +16,11 @@ public class QuestionSCript : MonoBehaviour
     GameObject Abutton4;
 
     GameObject pic;
-    Vector3 picPos;
+    
     public Image questionImage;
     public Text questionText;
-    public Image answerImage;
 
-    public Image boll4;
+    
     // Use this for initialization
     void Start()
     {
@@ -35,97 +34,56 @@ public class QuestionSCript : MonoBehaviour
         Abutton3 = GameObject.Find("AButton3");
         Abutton4 = GameObject.Find("AButton4");
 
-        pic = GameObject.Find("SolvedImage");
-        
-        picPos = pic.transform.position;
-        boll4.enabled = false;
-
         questionImage.enabled = false;
         questionText.enabled = false;
-        answerImage.enabled = false;
         Abutton1.SetActive(false);
         Abutton2.SetActive(false);
         Abutton3.SetActive(false);
         Abutton4.SetActive(false);
-        Debug.Log(picPos);
+       
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        //QuestionPressed();
+
     }
 
+    //bottom skåne
     public void Question1Pressed()
     {
-        //if (Input.GetMouseButtonDown(0))
-        //{
-       
-        pic.transform.localScale -= new Vector3(0.5f, 0.5f, 1);
-        Debug.Log("button pressed");
         questionImage.enabled = true;
         questionText.enabled = true;
-        answerImage.enabled = true;
         Abutton1.SetActive(true);
         Abutton2.SetActive(true);
         Abutton3.SetActive(true);
         Abutton4.SetActive(true);
-        //}
-        picPos = new Vector3(pic.transform.position.x + 125f, pic.transform.position.y + 125f, pic.transform.position.z);
     }
 
+    //mid mitten av sverige
     public void Question2Pressed()
     {
-        //if (Input.GetMouseButtonDown(0))
-        //{
-        picPos = new Vector3(pic.transform.position.x + 2f, pic.transform.position.y + 2f, pic.transform.position.z);
-        pic.transform.localScale -= new Vector3(0.5f, 0.5f, 1);
-        Debug.Log("button pressed");
         questionImage.enabled = true;
         questionText.enabled = true;
-        answerImage.enabled = true;
         Abutton1.SetActive(true);
         Abutton2.SetActive(true);
         Abutton3.SetActive(true);
         Abutton4.SetActive(true);
-        Debug.Log(picPos);
-        //}
     }
 
+
+    //top norra av sverige
     public void Question3Pressed()
     {
-        //if (Input.GetMouseButtonDown(0))
-        //{
-        picPos = new Vector3(pic.transform.position.x + 2f, pic.transform.position.y + 2f, pic.transform.position.z);
-        pic.transform.localScale -= new Vector3(0.5f, 0.5f, 1);
-        Debug.Log("button pressed");
         questionImage.enabled = true;
         questionText.enabled = true;
-        answerImage.enabled = true;
         Abutton1.SetActive(true);
         Abutton2.SetActive(true);
         Abutton3.SetActive(true);
         Abutton4.SetActive(true);
-        //}
     }
 
-    public void Question4Pressed()
-    {
-        //if (Input.GetMouseButtonDown(0))
-        //{
-        picPos = new Vector3(pic.transform.position.x + 2f, pic.transform.position.y + 2f, pic.transform.position.z);
-        pic.transform.localScale -= new Vector3(0.5f, 0.5f, 1);
-        Debug.Log("button pressed");
-        questionImage.enabled = true;
-        questionText.enabled = true;
-        answerImage.enabled = true;
-        Abutton1.SetActive(true);
-        Abutton2.SetActive(true);
-        Abutton3.SetActive(true);
-        Abutton4.SetActive(true);
-        //}
-    }
 
     public void CheckAnswer()
     {
